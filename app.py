393d98821e825c095e8a10f10f3ee1a1cce2d6d0
fauzul52 <fauzulrahman328@gmail.com>
2024-05-14 16:19:13 +0700

    first github project

---
+++ app.py
@@ -0,0 +1,7 @@
+def luas_segitiga():
+    a = int(input("masukkan Alas segitiga: "))
+    t= int(input("masukkan Tinggi segitiga: "))
+    luas = a*t / 2
+    print("Luas segitig adalah: ", luas)
+
+luas_segitiga()
