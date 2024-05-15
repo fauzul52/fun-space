3fbc4ad48a7997e99075f2bb7706d2004e0f938d
fauzul52 <fauzulrahman329@gmail.com>
2024-05-15 12:31:50 +0700

    source code luas persegi panjang

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#hitung luas segitiga
 def luas_segitiga():
     a = int(input("masukkan Alas segitiga: "))
     t= int(input("masukkan Tinggi segitiga: "))
@@ -5,3 +6,12 @@ def luas_segitiga():
     print("Luas segitig adalah: ", luas)
 
 luas_segitiga()
+
+#hitung luas persegi panjang
+def persegi_panjang():
+    p = int(input("masukkan panjang persegi: "))
+    l = int(input("masukkan luas persegi: "))
+    luas = p*l 
+    print("luas persegi panjang: ", luas)
+
+persegi_panjang()
